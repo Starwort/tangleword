de295c4164afebb6986b373c2e2a944cacba4528
starwort <tcphone93@gmail.com>
2024-03-15 00:36:53 +0000

    Improve Custom Puzzles page

--- src/pages/CustomPuzzles.tsx
+++ src/pages/CustomPuzzles.tsx
@@ -1,29 +1,64 @@
-import {List, ListItem, ListItemButton, ListItemText} from "@suid/material";
+import {Card, CardHeader, Divider, List, ListItem, ListItemButton, ListItemText, ListSubheader} from "@suid/material";
 import {PageProps} from "./PageProps";
-const CUSTOM_PUZZLES: [string, string][] = [
-    ["Guardian Example", "animal,0,3,6;verb,1,3,7;transportation,2,3,4;food,1,5,7;furniture,0,5,6;verb,2,4,7;000d8041d1d221b1"],
-    ["Guardian Puzzle 1", "Great Dane,0,4,6;sponge,1,5,7;hook,2,5,6;appreciate,0,3,7;capacious,1,3,6;Edgar A,2,4,5;00188e1478c3610c"],
-    ["Guardian Puzzle 2", "murder,0,4,6;exclamation,1,5,7;furnishing,2,5,6;pronoun,0,3,7;computer term,1,3,6;female name,2,4,5;001af2df7e03c484"],
-    ["Guardian Puzzle 3", "time,0,4,6;captive,1,5,7;male name,2,5,6;noise,0,3,7;object,1,3,6;city,2,4,5;0009297419c64ad9"],
-    ["cheddarmonk's Puzzle", "residue,0,2,6;consumed,0,5,7;receptacle,1,3,4;0 or 1,1,3,5;the cat's mother,2,6,7;generic ordinal,4,5,6;0008741ae4183550"],
-    ["Tangleword Original Puzzle 1", "verb,0,4,5;exclamation,0,1,7;poetic+or+archaic+verb,1,3,6;Egyptian+mother+goddess,2,4,5;French+sea,2,6,7;hardware,0,3,4;0017273f203112bf"],
-];
+
+const CUSTOM_PUZZLES: Record<string, [string, string][]> = {
+    "Tangleword Original": [
+        ["Puzzle 1", "verb,0,4,5;exclamation,0,1,7;poetic+or+archaic+verb,1,3,6;Egyptian+mother+goddess,2,4,5;French+sea,2,6,7;hardware,0,3,4;0017273f203112bf"],
+    ],
+    "The Guardian": [
+        ["Example", "animal,0,3,6;verb,1,3,7;transportation,2,3,4;food,1,5,7;furniture,0,5,6;verb,2,4,7;000d8041d1d221b1"],
+        ["Puzzle 1", "Great Dane,0,4,6;sponge,1,5,7;hook,2,5,6;appreciate,0,3,7;capacious,1,3,6;Edgar A,2,4,5;00188e1478c3610c"],
+        ["Puzzle 2", "murder,0,4,6;exclamation,1,5,7;furnishing,2,5,6;pronoun,0,3,7;computer term,1,3,6;female name,2,4,5;001af2df7e03c484"],
+        ["Puzzle 3", "time,0,4,6;captive,1,5,7;male name,2,5,6;noise,0,3,7;object,1,3,6;city,2,4,5;0009297419c64ad9"],
+    ],
+    "User-Submitted": [
+        ["cheddarmonk's Puzzle", "residue,0,2,6;consumed,0,5,7;receptacle,1,3,4;0 or 1,1,3,5;the cat's mother,2,6,7;generic ordinal,4,5,6;0008741ae4183550"],
+    ],
+};
 
 export function CustomPuzzles(props: PageProps) {
     return <>
-        <List>
-            {CUSTOM_PUZZLES.map(([name, puzzleDesc]) => <ListItem disablePadding>
+        <Card sx={{
+            maxWidth: 600,
+            width: "100%",
+            textAlign: "center"
+        }}>
+            <CardHeader
+                title="Custom Puzzles"
+                subheader="Puzzles made by humans"
+            />
+            <List>
+                {Object.entries(CUSTOM_PUZZLES).map(([category, puzzles]) => (
+                    <>
+                        <ListSubheader sx={{backgroundColor: "transparent"}}>
+                            {category}
+                        </ListSubheader>
+                        {puzzles.map(([name, puzzleDesc]) => <ListItem disablePadding>
+                            <ListItemButton
+                                component="a"
+                                href={window.location.pathname + "?puzzle=" + puzzleDesc}
+                                onClick={event => {
+                                    event.preventDefault();
+                                    props.setPage("play", "puzzle=" + puzzleDesc);
+                                }}
+                            >
+                                <ListItemText primary={name} />
+                            </ListItemButton>
+                        </ListItem>)}
+                    </>
+                ))}
+                <Divider />
                 <ListItemButton
                     component="a"
-                    href={window.location.pathname + "?puzzle=" + puzzleDesc}
+                    href={window.location.pathname + "?page=designer"}
                     onClick={event => {
                         event.preventDefault();
-                        props.setPage("play", "puzzle=" + puzzleDesc);
+                        props.setPage("designer");
                     }}
                 >
-                    <ListItemText primary={name} />
+                    <ListItemText primary="Design your own" />
                 </ListItemButton>
-            </ListItem>)}
-        </List>
+            </List>
+        </Card>
     </>;
 }
